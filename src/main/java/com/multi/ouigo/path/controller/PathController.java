516f0d9229151e7099f1e781ae645611de4ec93f
KIMGWANGHO <kkh185923@naver.com>
2025-11-11 00:59:55 +0900

    feat: PathController /tourist , /tourist-detail 추가

---
+++ src/main/java/com/multi/ouigo/path/controller/PathController.java
@@ -0,0 +1,23 @@
+package com.multi.ouigo.path.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class PathController {
+    @GetMapping("/tourist")
+    public String showTouristPage() {
+
+        return "tourist";
+    }
+
+
+    @GetMapping("/tourist-detail")
+    public String showTouristDetailPage(@RequestParam Long id, Model model) {
+        model.addAttribute("touristId", id);
+        return "touristDetail";
+    }
+
+}
