5578ffa1ad71557ba00124f146763e51ddb5f8b2
kyungsoo <yksoo00@naver.com>
2025-11-11 00:15:55 +0900

    feat: EntityGraph 사용 및 specification을 사용한 page findAll 메서드 구현

--- src/main/java/com/multi/ouigo/domain/recruit/repository/RecruitRepository.java
+++ src/main/java/com/multi/ouigo/domain/recruit/repository/RecruitRepository.java
@@ -1,8 +1,16 @@
 package com.multi.ouigo.domain.recruit.repository;
 
 import com.multi.ouigo.domain.recruit.entity.Recruit;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface RecruitRepository extends JpaRepository<Recruit, Long> {
+public interface RecruitRepository extends JpaRepository<Recruit, Long>,
+    JpaSpecificationExecutor<Recruit> {
 
+    @EntityGraph(attributePaths = {"member"})
+    Page<Recruit> findAll(Specification<Recruit> spec, Pageable pageable);
 }
