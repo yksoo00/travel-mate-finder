086b7aaccdb74d273e4b5cde838dc4871cb0abc2
DESKTOP-P7D06C6\hbk49 <hbok491@gmail.com>
2025-11-12 17:03:12 +0900

    feat: 마이페이지 관련 로직 추가

--- src/main/java/com/multi/ouigo/path/controller/PathController.java
+++ src/main/java/com/multi/ouigo/path/controller/PathController.java
@@ -3,6 +3,7 @@ package com.multi.ouigo.path.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -37,5 +38,43 @@ public class PathController {
     public String layout() {
         // templates/recruit/recruitList.html
         return "layout";
+    }
+
+    // 마이페이지 홈
+
+    @GetMapping("/myPage")
+    public String myPageHome(@RequestParam(required = false) Long memberNo, Model model) {
+        if (memberNo != null) {
+            model.addAttribute("memberNo", memberNo);
+        }
+        return "myPage/myPageHome";
+    }
+
+
+     // 프로필 수정
+
+    @GetMapping("/myPage/profileEdit")
+    public String profileEdit(@RequestParam Long memberNo, Model model) {
+        model.addAttribute("memberNo", memberNo);
+        return "myPage/profileEdit";
+    }
+
+
+    // 여행 일정 등록
+
+    @GetMapping("/myPage/tripCreate")
+    public String tripCreate(@RequestParam Long memberNo, Model model) {
+        model.addAttribute("memberNo", memberNo);
+        return "myPage/tripCreate";
+    }
+
+
+     // 여행 일정 수정
+
+    @GetMapping("/myPage/tripEdit")
+    public String tripEdit(@RequestParam Long memberNo, @RequestParam Long tripId, Model model) {
+        model.addAttribute("tripId", tripId);
+        model.addAttribute("memberNo", memberNo);
+        return "myPage/tripEdit";
     }
 }
