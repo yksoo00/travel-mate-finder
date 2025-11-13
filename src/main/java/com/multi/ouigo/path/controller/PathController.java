5ef49f17973b099b14504e317cf85bea22ec58e1
kyungsoo <yksoo00@naver.com>
2025-11-13 14:19:51 +0900

    feat: view 경로 추가

--- src/main/java/com/multi/ouigo/path/controller/PathController.java
+++ src/main/java/com/multi/ouigo/path/controller/PathController.java
@@ -3,6 +3,7 @@ package com.multi.ouigo.path.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -14,6 +15,10 @@ public class PathController {
         return "tourist";
     }
 
+    @GetMapping("/loginForm")
+    public String loginForm() {
+        return "loginForm";
+    }
 
     @GetMapping("/tourist-detail")
     public String showTouristDetailPage(@RequestParam Long id, Model model) {
@@ -27,6 +32,12 @@ public class PathController {
         return "recruit/recruitListPage";
     }
 
+    @GetMapping("/recruit/recruitUpdatePage")
+    public String recruitUpdatePage() {
+
+        return "recruit/recruitUpdatePage";
+    }
+
     @GetMapping("/myPage/profilePage")
     public String profilePage() {
         // templates/recruit/recruitList.html
@@ -34,8 +45,38 @@ public class PathController {
     }
 
     @GetMapping("/layout")
-    public String layout() {
-        // templates/recruit/recruitList.html
+    public String layout(Model model) {
+        model.addAttribute("pageFragment", "recruit/recruitListPage");
+        return "layout";
+    }
+
+    @GetMapping("/recruit/{id}")
+    public String recruitDetailPage(@PathVariable(name = "id") Long id, Model model) {
+        model.addAttribute("recruitId", id);
+        System.out.println(id);
+        model.addAttribute("pageFragment", "recruit/recruitDetailPage");
+        return "layout"; // templates/recruit/recruitDetailPage.html
+    }
+
+    @GetMapping("/recruit/recruitApprovalPage")
+    public String recruitApprovalPage() {
+        return "recruit/recruitApprovalPage";
+
+    }
+
+    @GetMapping("/recruit/recruitCreatePage")
+    public String recruitCreatePage(Model model) {
+        model.addAttribute("pageFragment", "recruit/recruitCreatePage");
+        model.addAttribute("pageTitle", "모집글 작성");
+        return "layout";
+    }
+
+
+    @GetMapping("/recruit/recruitUpdatePage/{recruitId}")
+    public String recruitUpdatePage(@PathVariable Long recruitId, Model model) {
+        model.addAttribute("pageFragment", "recruit/recruitUpdatePage");
+        model.addAttribute("pageTitle", "모집글 수정");
+        model.addAttribute("recruitId", recruitId);
         return "layout";
     }
 }
